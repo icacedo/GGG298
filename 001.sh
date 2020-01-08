c8a9595c7d8b5554f0f0c6c1686aec1e7a27bc8f
icacedo <55859921+icacedo@users.noreply.github.com>
2020-01-08 12:05:07 -0800

    Update 001.sh

--- 001.sh
+++ 001.sh
@@ -1,2 +1,14 @@
-## Jan 8, 2020
+## Week 1: Jan 8, 2020
 ## http://bit.ly/ggg298syllabus
+## no homework this week, but homework will be due next week
+## generating a public and private ssh key
+## generate ssh keys in windows with mobaxterm
+## give permission to execute in .ssh/
+## moved my ssh keys into .ssh/ from ssh/ and deleted ssh/
+  chmod -R +X .ssh/
+## generate a key file pair 
+  ssh-keygen –t rsa
+## the public key is given to farm (lock)
+## use the private key to log in (key)
+## in r markdown, can use echo=TRUE/FALSE to include or exclude code
+
