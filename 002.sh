c89ce3d9b612bb297059be1bd22a5953b2d771a0
icacedo <55859921+icacedo@users.noreply.github.com>
2020-01-15 10:31:33 -0800

    Update 002.sh

--- 002.sh
+++ 002.sh
@@ -1,3 +1,13 @@
 ## January 15, 2020
 ## github.com/ngs-docs/2020-GGG298
-## 
+## click on binder, then go to launch binder and start a terminal session
+## ls commands that flags directories and executables
+  ls -Fla
+  ls -F 
+  ls -la
+  ls -l
+## can combine commands
+## -h turns storage numbers to kb/gb
+## -t sorts by time
+  ls -aFlht
+  
